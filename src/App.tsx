27f2ec6208819534b31c9665b8d53cdef81258b0
FrankenDeba <debashishrambhola@gmail.com>
2022-12-22 01:23:45 +0530

    testing ci cd change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from "react";
 import "./styles.css";
 
-export default function App() : ReactElement {
+export default function App(): ReactElement {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>github changes</p>
     </div>
   );
 }
