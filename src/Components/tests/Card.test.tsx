f842142b722b2e5e971035ee5131dec91d8df3ef
FrankenDeba <debashishrambhola@gmail.com>
2022-12-22 01:23:12 +0530

    added tests

--- src/Components/tests/Card.test.tsx
+++ src/Components/tests/Card.test.tsx
@@ -1,12 +1,39 @@
-import { render } from "@testing-library/react";
+import React from "react";
+import { act, fireEvent, render } from "@testing-library/react";
 import Card from "../Card";
 
-describe("Testing render of cards: ", async () => {
-  test("Testing card header: ", () => {
-    const container = render(
+describe("Testing render of cards: ", () => {
+  // const container = await render(
+  //   <Card name="debashis" age={26} striked={true} setStriked={jest.fn()} />
+  // );
+
+  test("Testing card header & texts: ", async () => {
+    const container = await render(
       <Card name="debashis" age={26} striked={true} setStriked={jest.fn()} />
     );
     const cardHeader = container.getByTestId("card-header");
     expect(cardHeader.textContent).toBe("Welcome to the card");
+    const cardName = container.getByTestId("card-name");
+    expect(cardName.textContent).toBe("debashis");
+    const cardAge = container.getByTestId("card-age");
+    expect(cardAge.textContent).toBe("26");
+  });
+
+  test("Testing card strike status change onClick: ", async () => {
+    // const strikeBtn = await act(() => container.queryByTestId("card-striker"));
+    // const strikeBtn = screen.querySelector('[data-testid=""]')
+    const container = await render(
+      <Card name="debashis" age={26} striked={true} setStriked={jest.fn()} />
+    );
+    await act(async () => {
+      const strikeBtn = container.getAllByTestId("card-striker")[0];
+      fireEvent.click(strikeBtn);
+
+      expect(strikeBtn.textContent).toBe("X");
+      // fireEvent.click(strikeBtn);
+      // expect(strikeBtn.textContent).toBe("X");
+    });
+    // fireEvent.click(strikeBtn);
+    // expect(strikeBtn.textContent).toBe("O");
   });
 });
