b84b25d4213e274554ae20946f4926602bdb5b4f
FrankenDeba <debashishrambhola@gmail.com>
2022-12-22 01:30:23 +0530

    connected app property

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import Card from "./Components/Card";
 import "./styles.css";
 
 export default function App(): ReactElement {
@@ -7,6 +8,7 @@ export default function App(): ReactElement {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>github changes</p>
+      <Card name="debashis" age={26} striked={true} setStriked={() => {}} />
     </div>
   );
 }
